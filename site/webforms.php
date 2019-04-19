399e554994380db1a79031f83043aa2ef4563a6a
unknown <jimmychiucm@gmail.com>
2019-04-19 23:55:39 +0800

    My first page

---
+++ site/webforms.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+	<title>Reading data from datetime controls</title>
+</head>
+<body>
+		<h1>Reading data from datetime controls</h1>
+		You entered:
+		<?php echo $_REQUEST["datetime"];?>
+</body>
+</html>
